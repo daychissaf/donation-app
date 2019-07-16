020b427ff37c9a0e6c6365a51149ffe4be33a75e
asus <a.naji9710@gmail.com>
2019-07-16 11:55:40 +0100

    swagger api info

--- donation-web/src/main/java/com/donation/conf/SwaggerConfig.java
+++ donation-web/src/main/java/com/donation/conf/SwaggerConfig.java
@@ -14,7 +14,7 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -25,68 +25,62 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
-   public static final String AUTHORIZATION_HEADER = "Authorization";
-   public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
-   private final Logger log = LoggerFactory.getLogger(SwaggerConfig.class);
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+    private final Logger log = LoggerFactory.getLogger(SwaggerConfig.class);
 
-   @Bean
-   public Docket swaggerSpringfoxDocket() {
-      log.debug("Starting Swagger");
-      Contact contact = new Contact(
-              "Matyas Albert-Nagy",
-              "https://justrocket.de",
-              "matyas@justrocket.de");
+    @Bean
+    public Docket swaggerSpringfoxDocket() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Donation API",
+                "This application allows client to connect to donation app",
+                "0.0.1",
+                "TERMS OF SERVICE URL",
+                new Contact("NAME", "URL", "EMAIL"),
+                "Donation",
+                "Donation link",
+                Collections.emptyList()
+        );
 
-      List<VendorExtension> vext = new ArrayList<>();
-      ApiInfo apiInfo = new ApiInfo(
-              "Backend API",
-              "This is the best stuff since sliced bread - API",
-              "6.6.6",
-              "https://justrocket.de",
-              contact,
-              "MIT",
-              "https://justrocket.de",
-              vext);
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .pathMapping("/")
+                .apiInfo(ApiInfo.DEFAULT)
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .ignoredParameterTypes(Pageable.class)
+                .ignoredParameterTypes(java.sql.Date.class)
+                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false);
 
-      Docket docket = new Docket(DocumentationType.SWAGGER_2)
-              .apiInfo(apiInfo)
-              .pathMapping("/")
-              .apiInfo(ApiInfo.DEFAULT)
-              .forCodeGeneration(true)
-              .genericModelSubstitutes(ResponseEntity.class)
-              .ignoredParameterTypes(Pageable.class)
-              .ignoredParameterTypes(java.sql.Date.class)
-              .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
-              .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
-              .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
-              .securityContexts(Lists.newArrayList(securityContext()))
-              .securitySchemes(Lists.newArrayList(apiKey()))
-              .useDefaultResponseMessages(false);
-
-      docket = docket.select()
-              .paths(regex(DEFAULT_INCLUDE_PATTERN))
-              .build();
-      return docket;
-   }
+        docket = docket.select()
+                .paths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+        return docket;
+    }
 
 
-   private ApiKey apiKey() {
-      return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-   }
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
 
-   private SecurityContext securityContext() {
-      return SecurityContext.builder()
-              .securityReferences(defaultAuth())
-              .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
-              .build();
-   }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
 
-   List<SecurityReference> defaultAuth() {
-      AuthorizationScope authorizationScope
-              = new AuthorizationScope("global", "accessEverything");
-      AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-      authorizationScopes[0] = authorizationScope;
-      return Lists.newArrayList(
-              new SecurityReference("JWT", authorizationScopes));
-   }
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
 }
