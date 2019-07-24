8f0514bad309f96f72815c5f4ecb98d2f9a156f1
abirelhalimi <abirelhalimi@gmail.com>
2019-07-24 10:18:54 +0100

    VideoService, VideoStreamingController updated

--- donation-web/src/main/java/com/donation/web/VideoStreamingController.java
+++ donation-web/src/main/java/com/donation/web/VideoStreamingController.java
@@ -2,7 +2,6 @@ package com.donation.web;
 
 import com.donation.VideoService;
 import com.donation.crud.VideoCrudService;
-import com.donation.donor.model.Video;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api/stream")
@@ -27,11 +23,6 @@ public class VideoStreamingController {
 
     @GetMapping("/{idProject}")
     public StreamingResponseBody stream(@PathVariable Long idProject) throws FileNotFoundException {
-        Video video = videoCrudService.getRandomVideo(idProject);
-        File videoFile = new File(video.getLink());
-        final InputStream videoFileStream = new FileInputStream(videoFile);
-        return (os) -> {
-            videoService.readAndWrite(videoFileStream, os);
-        };
+        return videoService.stream(idProject);
     }
 }
