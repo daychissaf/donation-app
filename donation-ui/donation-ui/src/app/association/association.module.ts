1d2560e5965d345806d5e878a798a9c89cd27b40
Abir EL HALIMI <45515682+abirelhalimi@users.noreply.github.com>
2019-10-30 22:43:19 +0100

    crud update

--- donation-ui/donation-ui/src/app/association/association.module.ts
+++ donation-ui/donation-ui/src/app/association/association.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AssociationListComponent } from './association-list/association-list.component';
-import { AssociationDetailComponent } from './association-detail/association-detail.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AssociationListComponent} from './association-list/association-list.component';
+import {AssociationDetailComponent} from './association-detail/association-detail.component';
 import {RouterModule} from "@angular/router";
-import { AssociationAddComponent } from './association-add/association-add.component';
-import { AssociationEditComponent } from './association-edit/association-edit.component';
+import {AssociationAddComponent} from './association-add/association-add.component';
+import {AssociationEditComponent} from './association-edit/association-edit.component';
 import {FormsModule} from "@angular/forms";
 
 
@@ -14,7 +14,8 @@ import {FormsModule} from "@angular/forms";
   imports: [
     CommonModule,
     RouterModule,
-    FormsModules
+    FormsModule,
   ]
 })
-export class AssociationModule { }
+export class AssociationModule {
+}
